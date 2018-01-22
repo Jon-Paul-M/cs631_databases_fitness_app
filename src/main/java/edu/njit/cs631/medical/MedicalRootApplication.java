612463b6338a113edaeb0999941e55b2176fa11b
Jon-Paul Marnien <jonpaul.marnien@gmail.com>
2018-01-22 13:46:26 -0500

    Initial commit

---
+++ src/main/java/edu/njit/cs631/medical/MedicalRootApplication.java
@@ -0,0 +1,12 @@
+package edu.njit.cs631.medical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalRootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalRootApplication.class, args);
+	}
+}
