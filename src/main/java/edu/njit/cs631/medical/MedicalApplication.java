7a3e649525c1545ee8dd0dd2d5d543ab1a7c47dc
Jon-Paul Marnien <jonpaul.marnien@gmail.com>
2018-01-27 22:46:37 -0500

    example home controller renders correctly

--- src/main/java/edu/njit/cs631/medical/MedicalApplication.java
+++ src/main/java/edu/njit/cs631/medical/MedicalApplication.java
@@ -2,8 +2,11 @@ package edu.njit.cs631.medical;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"edu.njit.cs631.medical.config",
+	            "edu.njit.cs631.example.config"})
 public class MedicalApplication {
 
 	public static void main(String[] args) {
