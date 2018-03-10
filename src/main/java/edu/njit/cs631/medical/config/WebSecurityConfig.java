c4da82a829a5df43c788d78f1ba07058186307dc
brownnrl <brownnrl@gmail.com>
2018-03-09 21:18:11 -0500

    (#2) User Authentication
    Moving some templates around to make user authentication work (dead simple
    templates following the tutorial).

--- src/main/java/edu/njit/cs631/medical/config/WebSecurityConfig.java
+++ src/main/java/edu/njit/cs631/medical/config/WebSecurityConfig.java
@@ -37,7 +37,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public UserDetailsService userDetailsService() {
         UserDetails user =
              User.withUsername("user")
-                .password("{noop}password")
+                .password("password")
                 .roles("USER")
                 .build();
 
