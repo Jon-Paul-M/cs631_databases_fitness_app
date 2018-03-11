60a343957540405d4179302a7f9dcae88db508ab
Jon-Paul Marnien <jonpaul.marnien@gmail.com>
2018-03-11 14:57:55 -0400

    personal can be patients, refactorization, pt 2

--- src/main/java/edu/njit/cs631/medical/data/entity/Personnel.java
+++ src/main/java/edu/njit/cs631/medical/data/entity/Personnel.java
@@ -2,21 +2,50 @@ package edu.njit.cs631.medical.data.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="PERSONNEL")
-public class Personnel extends Person {
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Personnel {
 
 	public Personnel() {
 		super();
 	}
+	
+	private Long id;
+	@Id
+    @GeneratedValue(strategy=GenerationType.SEQUENCE)
+    @Column(name="PERSONNEL_ID", nullable=false)
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	private Person person;
+	@JoinColumn(name = "PERSON_ID")
+	@OneToOne(fetch = FetchType.LAZY)
+	public Person getPerson() {
+		return person;
+	}
+	public void setPerson(Person person) {
+		this.person = person;
+	}
 
 	private PersonnelType personnelType;
 	@ManyToOne(optional=false)
-	@JoinColumn(name="personnel_type_id")
+	@JoinColumn(name="PERSONNEL_TYPE_ID")
 	public PersonnelType getPersonnelType() {
 		return personnelType;
 	}
