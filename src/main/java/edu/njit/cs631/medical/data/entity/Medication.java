06145eee51971432d32e4bef514245ee33135f4f
Jon-Paul Marnien <jonpaul.marnien@gmail.com>
2018-03-11 10:09:01 -0400

    MedicationInventory

--- src/main/java/edu/njit/cs631/medical/data/entity/Medication.java
+++ src/main/java/edu/njit/cs631/medical/data/entity/Medication.java
@@ -2,6 +2,7 @@ package edu.njit.cs631.medical.data.entity;
 
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -9,6 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -30,6 +32,15 @@ public class Medication {
 		this.id = id;
 	}
 	
+	private String code;
+	@Column(name="MEDICATION_CODE", nullable=false)
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+
 	private String name;
 	@Column(name="MEDICATION_NAME", nullable=false)
 	public String getName() {
@@ -46,5 +57,14 @@ public class Medication {
 	}
 	public void setPrescriptions(List<Prescription> prescriptions) {
 		this.prescriptions = prescriptions;
+	}
+	
+	private MedicationInventory medicationInventory;
+	@OneToOne(cascade = CascadeType.ALL, mappedBy = "medication", fetch = FetchType.LAZY)
+	public MedicationInventory getMedicationInventory() {
+		return medicationInventory;
+	}
+	public void setMedicationInventory(MedicationInventory medicationInventory) {
+		this.medicationInventory = medicationInventory;
 	}
 }
