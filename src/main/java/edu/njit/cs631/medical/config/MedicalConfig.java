936a4ca432c7c22dec052e0c9a7827ffcd213b1d
brownnrl <brownnrl@gmail.com>
2018-03-20 10:22:21 -0400

    (#2) User authentication
    Figuring out the magic sauce mix of annotations that won't cause the house
    of cards to come tumbling down.  :)
    
    EnableJpaRepositories / EntityScan added to medical config seemed to get
    the mapping to take for users.  Need to add tests from the initial data
    to see if it loaded properly.

--- src/main/java/edu/njit/cs631/medical/config/MedicalConfig.java
+++ src/main/java/edu/njit/cs631/medical/config/MedicalConfig.java
@@ -2,19 +2,21 @@ package edu.njit.cs631.medical.config;
 
 import java.util.Locale;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
-@ComponentScan({"edu.njit.cs631.example.medical.controller"})
+@EnableJpaRepositories
+@ComponentScan({"edu.njit.cs631.medical.web.controller"})
+@EntityScan(basePackages = {"edu.njit.cs631.medical.data.entity"})
 public class MedicalConfig {
-	
-	@Autowired
+
 
 	@Bean
 	public LocaleResolver localeResolver() {
--- src/main/java/edu/njit/cs631/medical/data/repository/security/UserRepository.java
+++ src/main/java/edu/njit/cs631/medical/data/repository/security/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Query("SELECT u FROM USER u, PERSON p WHERE u.PERSON_ID = p.PERSON_ID AND p.email = :email")
+    @Query("SELECT u FROM USER u, PERSON p WHERE  u.PERSON_ID = p.PERSON_ID AND p.EMAIL = :email")
     User findByEmail(@Param("email") String email);
 
     @Override
