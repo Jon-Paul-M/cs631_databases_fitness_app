b023ab3f9ea982910c3842c35de9f9ae1ad1d9cd
Jon-Paul Marnien <jonpaul.marnien@gmail.com>
2018-05-01 17:03:06 -0400

    payroll hourly function executes. need some data

--- src/main/java/edu/njit/cs631/fitness/data/manager/PayrollManager.java
+++ src/main/java/edu/njit/cs631/fitness/data/manager/PayrollManager.java
@@ -1,5 +1,8 @@
 package edu.njit.cs631.fitness.data.manager;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -21,9 +24,13 @@ public class PayrollManager {
 	}
 
 	public List<HourlyInstructorPayroll> loadHourlyPayroll() {
+		Timestamp beginning = Timestamp.valueOf(LocalDateTime.ofEpochSecond(0, 0, ZoneOffset.UTC));
+		Timestamp ending = Timestamp.valueOf(LocalDateTime.now());
 		List<HourlyInstructorPayroll> payrolls = entityManager.createQuery(
 				HourlyInstructorPayroll.GET_HOURLY_INSTRUCTOR_PAYROLL_JPQL, 
 				HourlyInstructorPayroll.class)
+				.setParameter("beginning", beginning)
+				.setParameter("ending", ending)
 				.getResultList();
 		return payrolls;
 	}
--- src/main/java/edu/njit/cs631/fitness/data/projection/HourlyInstructorPayroll.java
+++ src/main/java/edu/njit/cs631/fitness/data/projection/HourlyInstructorPayroll.java
@@ -10,14 +10,14 @@ public class HourlyInstructorPayroll {
 		    "   " + HourlyInstructorPayroll.class.getCanonicalName() + "(" +
 		    "       i.id, " +
 		    "       i.name, " +
-		    "       i.hourlyWage, " +
-		    "       INSTRUCTOR_HOURS_BY_TIME_INTERVAL(i.minutes) " +
+		    "       i.wage, " +
+		    "       INSTRUCTOR_HOURS_BY_TIME_INTERVAL(i.id, :beginning, :ending) " +
 		    "   ) " +
 		    "from HourlyInstructor i " +
 		    "";
 		    //"where p.createdOn > :fromTimestamp";
 	
-	public HourlyInstructorPayroll(Integer id, String name, BigDecimal hourlyWage, Long minutes) {
+	public HourlyInstructorPayroll(Integer id, String name, BigDecimal hourlyWage, Integer minutes) {
 		super();
 		this.id = id;
 		this.name = name;
@@ -33,7 +33,7 @@ public class HourlyInstructorPayroll {
 	private Integer id;
 	private String name;
 	private BigDecimal hourlyWage;
-	private Long minutes;
+	private Integer minutes;
 	
 	public BigDecimal grossPay() {
 		BigDecimal bigMinutes = new BigDecimal(minutes); 
@@ -59,11 +59,27 @@ public class HourlyInstructorPayroll {
 	public void setHourlyWage(BigDecimal wage) {
 		this.hourlyWage = wage;
 	}
-	public Long getMinutes() {
+	public Integer getMinutes() {
 		return minutes;
 	}
-	public void setMinutes(Long minutes) {
+	public void setMinutes(Integer minutes) {
 		this.minutes = minutes;
+	}
+
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("HourlyInstructorPayroll [id=");
+		builder.append(id);
+		builder.append(", name=");
+		builder.append(name);
+		builder.append(", hourlyWage=");
+		builder.append(hourlyWage);
+		builder.append(", minutes=");
+		builder.append(minutes);
+		builder.append("]");
+		return builder.toString();
 	}
 
 }
