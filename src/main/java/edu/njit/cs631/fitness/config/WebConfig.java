fbd557f0a9a78d06726ae21e859bfa3af4210cd5
Jon-Paul Marnien <jonpaul.marnien@gmail.com>
2018-05-02 22:09:42 -0400

    deleted all the example code;

--- src/main/java/edu/njit/cs631/fitness/config/WebConfig.java
+++ src/main/java/edu/njit/cs631/fitness/config/WebConfig.java
@@ -5,13 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-
-import edu.njit.cs631.example.web.interceptor.SessionInterceptor;
 
 @Configuration
 @EnableWebMvc
@@ -34,11 +31,6 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         registry.addViewController("/login").setViewName("home");
 	}
 
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(new SessionInterceptor()).addPathPatterns(new String[] { "/medical", "/medical/*" });
-		registry.addInterceptor(localeChangeInterceptor);
-	}
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
